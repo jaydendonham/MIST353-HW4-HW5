9ade9024ac1079af7c388c1e301840f0b7091f33
rcs00021 <142524906+rcs00021@users.noreply.github.com>
2024-03-26 14:51:36 -0400

    Update Vehicle.cs

--- TravelPortalAPI/Entities/Vehicle.cs
+++ TravelPortalAPI/Entities/Vehicle.cs
@@ -5,5 +5,9 @@ namespace TravelPortalAPI.Entities
 {
     public class Vehicle
     {
+    public int VIN { get; set; }
+    public string VMake { get; set; }
+    public string VModel { get; set; }
+    public int? VYear { get; set; }
     }
 }
