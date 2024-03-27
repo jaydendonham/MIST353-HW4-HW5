eb6d1720a63d140b8814987138d22e3a3ec36dd0
jmklr <jmk00022@mix.wvu.edu>
2024-03-27 12:45:35 -0400

    Added SQL Parameters to Driver.CS

--- TravelPortalAPI/Repositories/Driver.cs
+++ TravelPortalAPI/Repositories/Driver.cs
@@ -10,6 +10,21 @@ namespace TravelPortalAPI.Repositories
     public class Driver : IDriver
     {
    
-        
+	public async Task<List<Driver>> DriverRetrieveDetails(int did)
+	{
+	var param = new SqlParameter("@did", did);
+	
+	//Add DriverDetails here at some point
+	}
+
+        public async Task<int> AddDriver(Driver driver)
+	{
+	var DPosition = new SqlParameter("@DPosition", driver.Position);
+	var D_First = new SqlParameter("@D_First", driver.First);
+	var D_Last = new SqlParameter("@D_Last", driver.Last);
+	var D_Hometown = new SqlParameter("@D_Hometown", driver.Hometown);
+	var D_LNum = new SqlParameter("@D_LNum", driver.LNum);
+
+	//Add DriverDetails Here At Some Point
     }
 }
