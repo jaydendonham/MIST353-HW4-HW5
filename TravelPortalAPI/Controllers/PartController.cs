3ccd72535a8e5ed0f97d77c7ccde55bc746ce39b
rcs00021 <142524906+rcs00021@users.noreply.github.com>
2024-03-28 18:09:17 -0400

    Update PartController.cs

--- TravelPortalAPI/Controllers/PartController.cs
+++ TravelPortalAPI/Controllers/PartController.cs
@@ -1,3 +1,21 @@
 using TravelPortalAPI.Repositories;
 using TravelPortalAPI.Entities;
 using Microsoft.AspNetCore.Mvc;
+
+namespace TravelPortalAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PartController : ControllerBase
+    {
+        private readonly IPartRepository _partRepository;
+
+        public PartController(IPartRepository partRepository)
+        {
+            _partRepository = partRepository;
+        }
+
+        [HttpGet("{pNum}")]
+        public async Task<ActionResult<Part>> GetPart(int pNum)
+        {
+            var part = await _partRepository.GetPartByNumber(pNum);
