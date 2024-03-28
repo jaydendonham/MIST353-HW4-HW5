4791c4e9ac2f976e2e2c600ec2cc589d322aea0e
jmklr <jmk00022@mix.wvu.edu>
2024-03-28 14:39:22 -0400

    Created POI Entity

---
+++ TravelPortalAPI/Entities/POI.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TravelPortalAPI.Entities
+{
+    public class Driver
+    {
+	public int POI { get; set; }
+
+	[Required]
+	public string POI_Name { get; set; }
+
+	[Required]
+	public int POI_Add_Num { get; set; }
+
+	[Required]
+	public string POI_Street { get; set; }
+	
+	[Required]
+	public string POI_City { get; set; }
+	
+	[Required]
+	public string POI_State{ get; set; }
+
+	[Required]
+	public int POI_ZIP{ get; set; }
+    }
+}
