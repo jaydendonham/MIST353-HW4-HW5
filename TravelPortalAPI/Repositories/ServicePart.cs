f009033ced6d13ff52b044f35df41fdc534bad1e
rcs00021 <142524906+rcs00021@users.noreply.github.com>
2024-03-28 18:23:07 -0400

    Update ServicePart.cs

--- TravelPortalAPI/Repositories/ServicePart.cs
+++ TravelPortalAPI/Repositories/ServicePart.cs
@@ -1,1 +1,52 @@
+using TravelPortalAPI.Data;
+using TravelPortalAPI.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace TravelPortalAPI.Repositories
+{
+    public class PartRepository : IPartRepository
+    {
+        private readonly DbContextClass _dbContext;
+
+        public PartRepository(DbContextClass dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Part> GetPartByNumber(int pNum)
+        {
+            return await _dbContext.Parts.FindAsync(pNum);
+        }
+
+        public async Task<List<Part>> GetAllParts()
+        {
+            return await _dbContext.Parts.ToListAsync();
+        }
+
+        public async Task AddPart(Part part)
+        {
+            _dbContext.Parts.Add(part);
+            await _dbContext.SaveChangesAsync();
+        }
+     public async Task DeletePart(int pNum)
+        {
+            var part = await _dbContext.Parts.FindAsync(pNum);
+
+            if (part != null) //Will not allow delete of part not in the system
+            {
+                _dbContext.Parts.Remove(part);
+                await _dbContext.SaveChangesAsync();
+            }
+    }
+    public interface IPartRepository
+    {
+        Task<Part> GetPartByNumber(int pNum);
+        
+        Task<List<Part>> GetAllParts();
+        
+        Task AddPart(Part part);
+        
+        Task DeletePart(Part part); 
+    }
+}
 
