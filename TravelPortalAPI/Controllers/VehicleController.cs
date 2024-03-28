5fa84009c83531206c15b75ff4b1783f176e6bbf
rcs00021 <142524906+rcs00021@users.noreply.github.com>
2024-03-28 09:17:46 -0400

    Update VehicleController.cs

--- TravelPortalAPI/Controllers/VehicleController.cs
+++ TravelPortalAPI/Controllers/VehicleController.cs
@@ -2,17 +2,17 @@ using TravelPortalAPI.Repositories;
 using TravelPortalAPI.Entities;
 using Microsoft.AspNetCore.Mvc;
 
-
-[Route("api/[controller]")]
-[ApiController]
+//Ryan Sladic
+namespace TravelPortalAPI.Controllers
+        [Route("api/[controller]")]
+        [ApiController]
 public class VehicleController : ControllerBase
 {
     [HttpPost("manufacturer")]
     public IActionResult GetVehiclesByManufacturer([FromBody] ManufacturerInput input)
     {
-        // Implement logic to process the input
-        // For demonstration purposes, we'll just return a sample response
-        var response = $"You requested vehicles manufactured by {input.Manufacturer}.";
+
+        var response = $"You requested vehicles manufactured by {input.Manufacturer} manufacturer.";
         return Ok(response);
     }
 }
