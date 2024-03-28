cf5e722a938154d45f9b0cbcfe9fcb0d19c2d228
jmklr <jmk00022@mix.wvu.edu>
2024-03-28 13:54:16 -0400

    Added DriverService to program.cs

--- TravelPortalAPI/Program.cs
+++ TravelPortalAPI/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddScoped<ILocationService, LocationService>();
+builder.Services.AddScoped<ILocationService, LocationService>();
 builder.Services.AddDbContext<DbContextClass>();
 
 builder.Services.AddControllers();
