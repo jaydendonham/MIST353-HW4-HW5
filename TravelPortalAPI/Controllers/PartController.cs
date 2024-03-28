46a5c4dc356be5199965d515daf918cc0d739d0f
rcs00021 <142524906+rcs00021@users.noreply.github.com>
2024-03-28 18:13:30 -0400

    Update PartController.cs

--- TravelPortalAPI/Controllers/PartController.cs
+++ TravelPortalAPI/Controllers/PartController.cs
@@ -14,8 +14,22 @@ namespace TravelPortalAPI.Controllers
         {
             _partRepository = partRepository;
         }
-
         [HttpGet("{pNum}")]
         public async Task<ActionResult<Part>> GetPart(int pNum)
         {
             var part = await _partRepository.GetPartByNumber(pNum);
+            if (part == null)
+            {
+                return NotFound();
+            {
+            return part;
+        }
+
+        [HttpGet("PartName")] 
+        public async Task<ActionResult<IEnumerable<Part>>> GetAllParts()
+        {
+            var parts = await _partRepository.GetAllParts();
+            return Ok(parts);
+        }
+    }
+}
