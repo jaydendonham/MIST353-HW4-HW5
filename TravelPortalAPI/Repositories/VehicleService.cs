7a6197c84f0b9267170e8d89db986fc7d861c697
rcs00021 <142524906+rcs00021@users.noreply.github.com>
2024-03-28 18:03:37 -0400

    Update VehicleService.cs

--- TravelPortalAPI/Repositories/VehicleService.cs
+++ TravelPortalAPI/Repositories/VehicleService.cs
@@ -7,3 +7,29 @@ namespace TravelPortalAPI.Repositories
 {
     public class VehicleRepository : IVehicleService
     {
+private readonly DbContextClass _dbContextClass;
+        public VehicleService(DbContextClass dbContextClass)
+        {
+            _dbContextClass = dbContextClass;
+        }
+        public async Task<List<Vehicle>> VehicleGetDetails(int vehicleid)
+        public async Task<int> AddVehicle(Vehicle vehicle)
+        {
+          return await _dbContext.Vehicles.FindAsync(vehicleId);
+          } 
+           public async Task<int> AddVehicle(Vehicle vehicle)
+        {
+           
+            var vehicleID = new SqlParameter("@VehicleID", vehicle.ID);
+            var vehicleMake = new SqlParameter("@VehicleAddress", vehicle.Make);
+            var vehicleModel = new SqlParameter("@VehicleCity", vehicle.Model);
+            var vehicleYear = new SqlParameter("@VehicleState", vehicle.Year);
+            } 
+ await _dbContext.Vehicles.AddAsync(vehicle);
+            await _dbContext.SaveChangesAsync();
+            return vehicle.Id; 
+            }
+        }
+
+
+
