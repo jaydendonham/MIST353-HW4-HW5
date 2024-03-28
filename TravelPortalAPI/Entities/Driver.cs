855de2dc7500954fcfedfaeb95741100b42c0f82
jmklr <jmk00022@mix.wvu.edu>
2024-03-28 14:03:59 -0400

    Added requirements

--- TravelPortalAPI/Entities/Driver.cs
+++ TravelPortalAPI/Entities/Driver.cs
@@ -6,10 +6,20 @@ namespace TravelPortalAPI.Entities
     public class Driver
     {
 	public int DID { get; set; }
+
+	[Required]
 	public string DPosition { get; set; }
+
+	[Required]
 	public string D_First { get; set; }
+
+	[Required]
 	public string D_Last { get; set; }
+	
+	[Required]
 	public string D_Hometown { get; set; }
+	
+	[Required]
 	public string D_LNum { get; set; }
     }
 }
