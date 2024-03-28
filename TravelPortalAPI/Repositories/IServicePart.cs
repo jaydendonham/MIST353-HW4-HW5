1c608e6083584f0e708347ca488beb13c6a941b2
rcs00021 <142524906+rcs00021@users.noreply.github.com>
2024-03-28 18:32:45 -0400

    Update IServicePart.cs

--- TravelPortalAPI/Repositories/IServicePart.cs
+++ TravelPortalAPI/Repositories/IServicePart.cs
@@ -1,1 +1,12 @@
+//Ryan Sladic
 
+using System.Threading.Tasks;
+using TravelPortalAPI.Entities;
+
+namespace TravelPortalAPI.Repositories
+{
+    public interface IVehicleService
+    {
+        Task<List<Vehicle>> GetVehicleDetails(string make, string model, int year);
+    }
+}
