e768f28a8f4970a7db4c443f20e8ec7945240541
jmklr <jmk00022@mix.wvu.edu>
2024-03-28 15:23:38 -0400

    began work on body

--- TravelPortalAPI/Repositories/POIService.cs
+++ TravelPortalAPI/Repositories/POIService.cs
@@ -9,6 +9,15 @@ namespace TravelPortalAPI.Repositories
 {
     public class POI : IPOI
     {
+	private readonly DbContextClass _dbContextClass;
+	public POIService(DbContextClass dbContextClass)
+	{
+		_dbContextClass = dbContextClass;
+	}
 	
+	public async Task<List<Hotel>> POIGetDetails(string poi_state)
+	{
+	var param = new SqlParameter("@poi_state", poi_state);
+	var poiDetails //Finish later
     }
 }
