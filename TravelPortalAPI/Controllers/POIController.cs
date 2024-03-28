81a5087b22fb029fbc2b4a874ca183959900b1e6
jmklr <jmk00022@mix.wvu.edu>
2024-03-28 14:37:04 -0400

    Created POI Controller

---
+++ TravelPortalAPI/Controllers/POIController.cs
@@ -0,0 +1,16 @@
+﻿using TravelPortalAPI.Repositories;
+using TravelPortalAPI.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+
+//Josh Keller
+namespace TravelPortalAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class POIController : Controller
+    {
+
+
+    }
+}
