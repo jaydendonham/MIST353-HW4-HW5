82213e2b5238a0ddbf02ee5447e3f80b0b3d0da7
jaydendonham <jbd0017@mix.wvu.edu>
2024-04-09 15:27:15 -0400

    added changes to index and added careers page

---
+++ TravelPortal/Pages/Careers.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TravelPortal.Pages
+{
+    public class CareersModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
