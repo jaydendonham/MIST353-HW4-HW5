28da1ecfca84e469c5945ca5d93e7d55aee59300
jaydendonham <jbd0017@mix.wvu.edu>
2024-04-17 14:25:52 -0400

    created driverinfo, map, and partlibrary web pages

---
+++ TravelPortal/Pages/PartLibrary.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TravelPortal.Pages
+{
+    public class PartLibraryModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
