1e86d3c1c9fb2cb1021176217b35a154fda2c07b
MISTUser <MISTUser@lab8L4JUY>
2024-04-17 17:04:18 -0400

    Began adding POI API stuff

--- TravelPortal/wwwroot/js/site.js
+++ TravelPortal/wwwroot/js/site.js
@@ -9,3 +9,20 @@ async function displayCareersDetails(CareersID) {
     document.getElementById('CareersName').innerHTML = data[0].name;
     document.getElementById('CareersName').style.visibility = "visible";
 }
+
+async function displayPOIDetails(POI_ID) {
+    const response = await fetch(`https://localhost:7173/api/POI`)
+    const data = await response.json();
+    document.getElementById('POI_Name').innerHTML = data[0].name;
+    document.getElementById('POI_Name').style.visibility = "visible";
+    document.getElementById('POI_Add_Num').innerHTML = data[0].add_num;
+    document.getElementById('POI_Add_Num').style.visibility = "visible";
+    document.getElementById('POI_Street').innerHTML = data[0].street;
+    document.getElementById('POI_Street').style.visibility = "visible";
+    document.getElementById('POI_City').innerHTML = data[0].city;
+    document.getElementById('POI_City').style.visibility = "visible";
+    document.getElementById('POI_State').innerHTML = data[0].state;
+    document.getElementById('POI_State').style.visibility = "visible";
+    document.getElementById('POI_ZIP').innerHTML = data[0].zip;
+    document.getElementById('POI_ZIP').style.visibility = "visible";
+}
