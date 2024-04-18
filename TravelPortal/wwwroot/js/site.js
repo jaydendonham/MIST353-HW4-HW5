0c73cc066958722c5a54fbe0598e550954c1d93a
MISTUser <MISTUser@lab8L4JUY>
2024-04-18 02:29:24 -0400

    Re-formated map.cshtml. Corrections to site.js for poi api

--- TravelPortal/wwwroot/js/site.js
+++ TravelPortal/wwwroot/js/site.js
@@ -10,19 +10,20 @@ async function displayCareersDetails(CareersID) {
     document.getElementById('CareersName').style.visibility = "visible";
 }
 
-async function displayPOIDetails(POI_ID) {
-    const response = await fetch(`https://localhost:7173/api/POI`)
+async function displayPOIDetails(POI_State) {
+    const response = await fetch(`https://localhost:7173/api/POI/${POI_State}`);
     const data = await response.json();
-    document.getElementById('POI_Name').innerHTML = data[0].name;
+    document.getElementById('POI_Name').innerHTML = data[0].poI_Name;
     document.getElementById('POI_Name').style.visibility = "visible";
-    document.getElementById('POI_Add_Num').innerHTML = data[0].add_num;
+    document.getElementById('POI_Add_Num').innerHTML = data[0].poI_Add_Num;
     document.getElementById('POI_Add_Num').style.visibility = "visible";
-    document.getElementById('POI_Street').innerHTML = data[0].street;
+    document.getElementById('POI_Street').innerHTML = data[0].poI_Street;
     document.getElementById('POI_Street').style.visibility = "visible";
-    document.getElementById('POI_City').innerHTML = data[0].city;
+    document.getElementById('POI_City').innerHTML = data[0].poI_City;
     document.getElementById('POI_City').style.visibility = "visible";
-    document.getElementById('POI_State').innerHTML = data[0].state;
+    document.getElementById('POI_State').innerHTML = data[0].poI_State;
     document.getElementById('POI_State').style.visibility = "visible";
-    document.getElementById('POI_ZIP').innerHTML = data[0].zip;
+    document.getElementById('POI_ZIP').innerHTML = data[0].poI_ZIP;
     document.getElementById('POI_ZIP').style.visibility = "visible";
 }
+
