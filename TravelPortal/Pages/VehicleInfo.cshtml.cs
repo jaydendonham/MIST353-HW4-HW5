87d20c7a2bb6c198a7d5866cdfe589cd017c3db0
rcs00021 <142524906+rcs00021@users.noreply.github.com>
2024-04-18 11:33:02 -0400

    Add files via upload

---
+++ TravelPortal/Pages/VehicleInfo.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TravelPortal.Pages.Shared
+{
+    public class ResultsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
